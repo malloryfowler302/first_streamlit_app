b5beeee9f86c7110581d0b2c0a3576873a49e7a9
malloryfowler302 <57341144+malloryfowler302@users.noreply.github.com>
2022-10-21 18:11:31 -0400

    Update streamlit_app.py
    added a header 
    added three text boxes below the header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfest Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie') 
+stream.text('Hard-Boiled Free-Range Egg')
